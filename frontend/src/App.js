eabc92cbd4339473ad2395603c9a7498453301ef
Ashish2509 <ashish.2509.k@gmail.com>
2024-02-18 23:40:41 +0530

    Implemented Leaflet map

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,10 +5,10 @@ import Map from "./components/map";
 function App() {
 
   return (
-    <div className="d-flex container full align-items-center m-auto p-0">
-      <div className="row py-5 full mx-auto w-100 mb-5">
+    <div className="d-flex container align-items-center m-auto p-0">
+      <div className="row py-5 mx-auto w-100 mb-5">
         <div className="col-lg-7">
-          <Form/>
+          <Form />
         </div>
         <div className="col-lg-5">
           <Map/>
--- frontend/src/components/map.js
+++ frontend/src/components/map.js
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { MapContainer, TileLayer, useMap } from "react-leaflet";
+
+function Center() {
+  const map = useMap();
+  const [location, setLocation] = useState([28.7040592, 77.1024902]);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) =>{
+        setLocation([position.coords.latitude, position.coords.longitude])
+        map.flyTo(location, 10)
+      },
+      (error) => console.log(error),
+      { enableHighAccuracy: true }
+    );
+  });
+  
+  return null
+}
 
 function Map() {
+
   return (
-    <div>
-    
-    </div>
+    <MapContainer center={[28.7040592, 77.1024902]} zoom={10} scrollWheelZoom={true}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Center />
+    </MapContainer>
   );
 }
 
